d6a490a64f6f6f8de75f84387844a1e56fd5f3ea
Shivam <thisisshivam06@gmail.com>
2022-09-09 01:37:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+////////// Sum of Resistance in Series Circuits //////////
+
+function seriesResistance(arr) {
+  let rt = 0;
+  let ohmVar = "";
+  for (const elem of arr) {
+    rt = rt + elem;
+  }
+  if (rt > 1) {
+    ohmVar = "ohms";
+  } else {
+    ohmVar = "ohm";
+  }
+  return `${rt} ${ohmVar}`;
+}
+
+console.log(seriesResistance([16, 3.5, 6]));
+
+////////// How Much is True? //////////
+
+function countTrue(arr) {
+  let counter = 0;
+  for (const elem of arr) {
+    if (elem) {
+      counter++;
+    }
+  }
+  return counter;
+}
